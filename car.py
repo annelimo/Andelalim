4205831b13f035088c9f977fdc91cb9b9b130b09
annelimo <annshi.limz@gmail.com>
2017-03-10 17:00:42 +0300

    classcar1

---
+++ car.py
@@ -0,0 +1,16 @@
+class Car(object):
+    num_wheels = 4
+    gas = 30
+    headlights = 2
+    size = 'medium'
+
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+        self.color = 'No color yet. You need to paint me.'
+        self.wheels = Car.num_wheels
+        self.gas = Car.gas
+
+    def paint(self, color):
+        self.color = color
+        return self.make + ' ' + self.model + ' is now ' + color
